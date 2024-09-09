95cf47f2bf853c4d7d448950e2f309cb250f7401
Subhajit <dass8684@gmail.com>
2024-09-09 06:39:56 +0530

    final

---
+++ script.js
@@ -0,0 +1,10 @@
+let btn = document.getElementById("submit");
+
+// let Input = document.querySelectorAll("input").value;
+
+function feedback ()  {
+    alert("Your feedback has been submited");
+    }
+
+
+btn.addEventListener('click', feedback);
